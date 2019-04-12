e00a53bbf6bb53d4105f9f0d224305462fc8bbcd
Sarita Asthani <asthani77@outlook.com>
2019-04-12 23:51:36 +0545

    Initial commit

---
+++ app/src/main/java/com/example/tax_sarita/SalaryModel.java
@@ -0,0 +1,17 @@
+package com.example.tax_sarita;
+
+public class SalaryModel {
+    float salary;
+
+    public SalaryModel(float salary) {
+        this.salary = salary;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
