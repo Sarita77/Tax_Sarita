c39733565097440c327d667f13d0e5da354e52ff
Sarita Asthani <asthani77@outlook.com>
2019-04-13 09:07:01 +0545

    layout changes in the activity_main.xml and textname in the MainActivity.java

--- app/src/main/java/com/example/tax_sarita/MainActivity.java
+++ app/src/main/java/com/example/tax_sarita/MainActivity.java
@@ -8,37 +8,42 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-private EditText txtsalary;
-private Button btnCalculate;
-private TextView txtOutput;
-float salary;
+    private EditText txtsalary;
+    private Button btnCalculate;
+    private TextView txtOutput;
+    float salary;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initiate();
     }
-    private void initiate(){
-        txtsalary=findViewById(R.id.txtInput);
 
-        btnCalculate=findViewById(R.id.btnCalculate);
-        txtOutput=findViewById(R.id.txtOutput);
+    private void initiate() {
+        txtsalary = findViewById(R.id.etInput);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txtOutput = findViewById(R.id.tvOutput);
 
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-        txtOutput.setText("The tax for your salary is:" +calculate());
-            }}); }
-    private float calculate(){
-        salary=Float.parseFloat(txtsalary.getText().toString());
+                txtOutput.setText("The tax for your salary is:" + calculate());
+            }
+        });
+    }
+
+    private float calculate() {
+        salary = Float.parseFloat(txtsalary.getText().toString());
         float tax;
-        SalaryModel sm=new SalaryModel(salary*12);
-        if (sm.getSalary()>=1 && sm.getSalary()<=200000){
+        SalaryModel sm = new SalaryModel(salary * 12);
+        if (sm.getSalary() >= 1 && sm.getSalary() <= 200000) {
             tax = sm.getSalary() / 100;
-        }else if (sm.getSalary()>200000 && sm.getSalary()<=350000){
-            tax = (sm.getSalary() - 200000)*(15f/100) + (200000f/100);
-        }else {
-            tax = (sm.getSalary()-350000)*(25f/100) + (150000*15f)/100 + (200000f/100);
+        } else if (sm.getSalary() > 200000 && sm.getSalary() <= 350000) {
+            tax = (sm.getSalary() - 200000) * (15f / 100) + (200000f / 100);
+        } else {
+            tax = (sm.getSalary() - 350000) * (25f / 100) + (150000 * 15f) / 100 + (200000f / 100);
         }
         return tax;
-    }}
+    }
+}
